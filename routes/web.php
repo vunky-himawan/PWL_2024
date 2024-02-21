67c2d76ab1085359c010f6eec096684028ed5ef9
Vunky Himawan <2241720005@student.polinema.ac.id>
2024-02-21 21:11:20 +0700

    menambahkan langkah-langkah praktikum optional parameter karena terlewat

--- routes/web.php
+++ routes/web.php
@@ -31,24 +31,24 @@ use Illuminate\Support\Facades\Route;
 //     return "Hallo nama saya Vunky Himawan</br>saya Semester 4 di Politeknik Negeri Malang";
 // })->name('profile');
 
-// Route::get("/user/{name}", function (string $name) {
-//     return "Nama saya $name";
-// });
+Route::get("/user/{name?}", function (string $name = "John") {
+    return "Nama saya $name";
+});
 
 // Route::get("/posts/{post}/comments/{comment}", function (string $postId, string $commentId) {
 //     return "Post ke-$postId Komentar ke-$commentId";
 // });
 
-Route::get("/hello", [WelcomeController::class, 'hello']);
-Route::get('/', [HomeController::class, 'index']);
-Route::get("/about", [AboutController::class, 'about']);
-Route::get("/articles/{id}", [ArticleController::class, 'articles']);
+// Route::get("/hello", [WelcomeController::class, 'hello']);
+// Route::get('/', [HomeController::class, 'index']);
+// Route::get("/about", [AboutController::class, 'about']);
+// Route::get("/articles/{id}", [ArticleController::class, 'articles']);
 
-Route::resource("photos", PhotoController::class)->except([
-    'create',
-    'store',
-    'update',
-    'destroy'
-]);
+// Route::resource("photos", PhotoController::class)->except([
+//     'create',
+//     'store',
+//     'update',
+//     'destroy'
+// ]);
 
-Route::get("/greeting", [WelcomeController::class, 'greeting']);
+// Route::get("/greeting", [WelcomeController::class, 'greeting']);
